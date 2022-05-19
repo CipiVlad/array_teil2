ca4c94b4aecada8412cf50c37a642170ca1ac68d
CipiVlad <supercoder.cipi@gmail.com>
2022-05-19 13:47:13 +0200

    celsius

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,9 +70,136 @@ let join4 = meinText3.join("+");
 // lev2_2: sort() bigger numbers
 
 // Sort an array of numbers based on numerical value.
-let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
-console.log(numArray2);
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+// console.log(numArray2);
 
-let sortednumbers = numArray2.sort((a, b) => a - b);
+// let sortednumbers = numArray2.sort((a, b) => a - b);
+
+
+// -----------------------lev1_1: forEach()-----------------------------------------
+
+// 
+
+// let getraenke = [
+//     "Coca-Cola",
+//     "Apfelsaft",
+//     "Pepsi",
+//     "Traubensaft",
+//     "Sprite",
+//     "Orangensaft",
+//     "Red Bull Energy Drink",
+//     "Fanta"
+// ]
+
+// let myDrinks = getraenke.sort();
+// console.log(myDrinks);
+
+
+
+// myDrinks.forEach(element => {
+//     console.log(element);
+//     document.write(element + '<ul></ul>');
+
+// }
+
+// );
+
+
+
+
+// lev1_2: map()
+
+
+let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+]
+
+
+
+let upperDrinks = getraenke.map(element => {
+    return element.toUpperCase();
+})
+
+console.log(upperDrinks);
+
+
+// lev1_3: map()
+// Schreibe eine Funktion, die jeden Wert aus dem Array (siehe Kommentarbereich) mit 2 multipliziert und das Ergebnis sortiert.
+
+let array = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+];
+console.log(array);
+
+let array2 = array.map((element) => {
+    return (element * 2)
+});
+console.log(array2.sort());
+// let array3 = array2.sort();
+// console.log(array3);
+
+
+
+
+
+// lev1_4: map()
+
+
+// Schreibe eine Funktion, mit Hilfe von map(), die jede Temperatur in einem Array von Fahrenheit in Celsius umwandelt.
+
+
+// formel (32 °F − 32) × 5/9 = 0 °C
+
+let fahrenheit = [0, 32, 45, 50, 75, 80, 99, 120];
+let formel = (fahrenheit - 32) * (5 / 9);
+
+
+
+let toCelsius = fahrenheit.map((element) => {
+    return element - ((32) * (5 / 9)).toFixed();
+})
+
+console.log(toCelsius);
 
 
